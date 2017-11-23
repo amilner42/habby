ae166c68bc4de3892d6a630395d97ecfa7c3e7fd
Arie Milner <amilner42@gmail.com>
2017-11-22 16:19:44 -0800

    Add better logging in dev mode - http://lacinia.readthedocs.io/en/latest/tutorial/pedestal.html#some-configuration

--- api/project.clj
+++ api/project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.walmartlabs/lacinia "0.21.0"]
                  [com.walmartlabs/lacinia-pedestal "0.3.0"]
+                 [io.aviso/logging "0.2.0"]
                  [com.novemberain/monger "3.1.0"]
                  [slingshot "0.12.2"]
                  [clj-time "0.14.2"]])
