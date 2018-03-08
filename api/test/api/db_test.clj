ef4ebd3d24639f3cd9c88be3e09d1a3836ae3d36
Aly Thobani <athobani9@gmail.com>
2018-03-07 22:01:11 -0800

    Added proto-repl to project.clj as a dependency, and added a test file db_test.clj for api/db.clj

---
+++ api/test/api/db_test.clj
@@ -0,0 +1,110 @@
+(ns api.db-test
+  (:require [clojure.test :refer :all]
+            [clojure.data :refer [diff]]
+            [api.db :refer :all]
+            [monger.core :as mg]
+            [monger.db :as mdb]
+            [clj-time.core :as t]))
+
+(defonce test_conn (mg/connect))
+(defonce test_db (mg/get-db test_conn "test_db"))
+
+(def default_habit {:name "test habit"
+                    :description "test description"
+                    :unit_name_singular "test unit"
+                    :unit_name_plural "test units"
+                    :time_of_day :ANYTIME})
+
+(defn add-habit-to-test-db
+  "Add a habit to the test database"
+  [habit]
+  (add-habit {:db test_db :habit habit}))
+
+(deftest test-add-habit
+  (is (= 0 (count (get-habits {:db test_db}))))
+  (let [habit_1 (assoc default_habit
+                     :type_name "good_habit"
+                     :target_frequency {:type_name "total_week_frequency"
+                                        :week 6})
+        _ (add-habit-to-test-db habit_1)
+        all_habits (get-habits {:db test_db})]
+    (is (= 1 (count all_habits)))
+    (is (some #(nil? (first (diff habit_1 %))) all_habits) "Habit 1's inputted fields weren't set properly")
+    (is (every? #(= false (:suspended %)) all_habits) ":suspended field not set to false")
+    (is (every? #(not (nil? (:_id %))) all_habits) ":_id field not set"))
+  (let [habit_2 (assoc default_habit
+                       :type_name "bad_habit"
+                       :threshold_frequency {:type_name "every_x_days_frequency"
+                                             :days 4
+                                             :times 3})
+        _ (add-habit-to-test-db habit_2)
+        all_habits (get-habits {:db test_db})]
+    (is (= 2 (count all_habits)))
+    (is (some #(nil? (first (diff habit_1 %))) all_habits) "Habit 1's inputted fields weren't set properly")
+    (is (some #(nil? (first (diff habit_2 %))) all_habits) "Habit 2's inputted fields weren't set properly")
+    (is (every? #(= false (:suspended %)) all_habits) ":suspended field not set to false")
+    (is (every? #(not (nil? (:_id %))) all_habits) ":_id field not set")))
+
+(deftest test-get-specific-day-of-week-target-frequency-stats
+  (is (= (count (get-habits {:db test_db})) 0))
+  (let [habit_name "habit 1"
+        habit_desc "Good habit, specific day of week freq"
+        habit_type "good_habit"
+        habit_freq {:monday 1
+                    :tuesday 1
+                    :wednesday 1
+                    :thursday 1
+                    :friday 1
+                    :saturday 1
+                    :sunday 1
+                    :type_name "specific_day_of_week_frequency"}
+        habit_freq_type :target_frequency
+        _ (add-habit-to-test-db {:name habit_name
+                                 :description habit_desc
+                                 :type_name habit_type
+                                 habit_freq_type habit_freq})
+        all_habits (get-habits {:db test_db})
+        habit_from_db (first all_habits)
+        id (str (:_id habit_from_db))]
+    (is (= 1 (count all_habits)))
+    (are [value key] (= value (key habit_from_db))
+         habit_name :name
+         habit_desc :description
+         habit_freq habit_freq_type)
+    (is (not= "" id) "Habit should've been given an id")
+    (testing "Get frequency stats"
+      (testing "with no habit data"
+        (is (= [nil] (get-frequency-stats {:db test_db}))))
+      (testing "with a successful habit record yesterday"
+        (let [_ (set-habit-data {:db test_db
+                                 :habit_id id
+                                 :amount 4
+                                 :date-time (t/minus (t/today-at 0 0) (t/days 1))})
+               stats (first (get-frequency-stats {:db test_db}))]
+            (are [value key] (= value (key stats))
+                 id :habit_id
+                 1 :total_fragments
+                 1 :successful_fragments
+                 4 :total_done
+                 1 :fragment_streak)
+            (testing "and a failure habit record the day before"
+               (let [_ (set-habit-data {:db test_db
+                                        :habit_id id
+                                        :amount 0
+                                        :date-time (t/minus (t/today-at 0 0) (t/days 2))})
+                     stats (first (get-frequency-stats {:db test_db}))]
+                  (are [value key] (= value (key stats))
+                       id :habit_id
+                       2 :total_fragments
+                       1 :successful_fragments
+                       4 :total_done
+                       1 :fragment_streak))))))))
+
+(defn each-fixture
+  "Drop test database before and after each test"
+  [f]
+  (mdb/drop-db test_db)
+  (f)
+  (mdb/drop-db test_db))
+
+(use-fixtures :each each-fixture)
