7b256f134d7e927f6a86d577d1abc7170dfcdb9a
Aly Thobani <athobani9@gmail.com>
2018-05-14 19:33:19 -0700

    Refactored code into a `generate-random-datetime-d-days-later` function

--- api/test/api/dt_util_test.clj
+++ api/test/api/dt_util_test.clj
@@ -2,7 +2,6 @@
   (:require [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test :refer [deftest, testing, is]]
             [api.dt-util :refer [date-geq?, date-leq?, first-monday-before-datetime,
                                  days-spanned-between-datetimes, get-consecutive-datetimes]]
             [clj-time.core :as t]))
@@ -21,14 +20,14 @@
     (t/plus (t/date-time 2018 5 7)  ; May 7 2018 was a Monday
             (t/weeks weeks-to-add))))
 
-(defn get-later-datetime
-  "Returns a `DateTime` instance `days-to-add` days later than `dt`, at time `hour`:`minute`."
-  [dt days-to-add hour minute]
-  (-> dt
-      t/with-time-at-start-of-day
-      (t/plus (t/days days-to-add)
-              (t/hours hour)
-              (t/minutes minute))))
+(defn generate-random-datetime-d-days-later
+  "Returns a generator for a `DateTime` instance `d` days later than `dt`, with a random time."
+  [dt d]
+  (gen/let [hour generate-random-hour,
+            minute generate-random-minute]
+    (-> dt
+        t/with-time-at-start-of-day
+        (t/plus (t/days d) (t/hours hour) (t/minutes minute)))))
 
 ; Generates a sorted two-element vector of DateTimes
 (def generate-two-random-sorted-datetimes
@@ -39,10 +38,8 @@
 ; Generates two random `DateTime`s, with `until-date` being `days-apart` days later than `from-date`. Returns a map of the values.
 (def generate-two-random-datetimes-with-days-apart
   (gen/let [from-date generate-random-datetime,
-            days-apart gen/nat,
-            until-date-hour generate-random-hour,
-            until-date-minute generate-random-minute]
-    (let [until-date (get-later-datetime from-date days-apart until-date-hour until-date-minute)]
+            days-apart gen/nat]
+    (let [until-date (gen/generate (generate-random-datetime-d-days-later from-date days-apart))]
       {:from-date from-date, :until-date until-date, :days-apart days-apart})))
 
 (defspec date-geq?-and-date-leq?-equal-dates-test
@@ -68,16 +65,16 @@
            (let [later-in-week-dt (t/plus monday-dt (t/days days-to-add))]
              (= monday-dt (first-monday-before-datetime later-in-week-dt)))))
 
-(deftest get-consecutive-datetimes-test
-         (testing "Today until today"
-                  (let [dt-a (t/today-at 3 3)
-                        dt-b (t/today-at 2 2)]
-                    (is (= [(t/today-at 0 0)] (get-consecutive-datetimes dt-a dt-b)))))
-         (testing "Today until tomorrow"
-                  (let [dt-a (t/today-at 3 3)
-                        dt-b (get-later-datetime dt-a 1 2 2)]
-                    (is (= [(t/today-at 0 0), (t/plus (t/today-at 0 0) (t/days 1))]
-                           (get-consecutive-datetimes dt-a dt-b))))))
+(defspec get-consecutive-datetimes-test
+         20
+         (let [today-at-start-of-day (t/today-at 0 0)]
+           (prop/for-all [random-today-dt-a (generate-random-datetime-d-days-later today-at-start-of-day 0)
+                          random-today-dt-b (generate-random-datetime-d-days-later today-at-start-of-day 0)
+                          random-tomorrow-dt (generate-random-datetime-d-days-later today-at-start-of-day 1)]
+             (and (= [today-at-start-of-day]
+                     (get-consecutive-datetimes random-today-dt-a random-today-dt-b))
+                  (= [today-at-start-of-day, (t/plus today-at-start-of-day (t/days 1))]
+                     (get-consecutive-datetimes random-today-dt-a random-tomorrow-dt))))))
 
 (defspec get-consecutive-datetimes-count-test
          10
